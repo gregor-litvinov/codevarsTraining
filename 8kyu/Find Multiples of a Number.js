2076c807dd4aa7d4e250e66fe35743d652110ca8
user <gregorlitvinov@gmail.com>
2023-02-21 00:03:09 +0200

    Get the mean of an array

--- 8kyu/Find Multiples of a Number.js
+++ 8kyu/Find Multiples of a Number.js
@@ -12,3 +12,14 @@ function findMultiples(integer, limit) {
   }
   return result;
 }
+
+
+/* function findMultiples(int,limit){
+  let result = []
+
+  for (let i = int; i<=limit ; i+=int)
+    result.push(i)
+
+  return result
+}
+*/
---
+++ 8kyu/Get the mean of an array.js
@@ -0,0 +1,22 @@
+//It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+//Return the average of the given array rounded down to its nearest integer.
+
+//The array will never be empty.
+
+function getAverage(marks) {
+  return Math.floor(marks.reduce((acc, curr) => acc + curr, 0) / marks.length);
+}
+
+
+/* function getAverage(marks){
+
+  var total = 0;
+  for(var i = 0; i < marks.length; i++){
+    total += marks[i];
+  }
+
+
+  return Math.floor(total / marks.length);
+}
+*/
