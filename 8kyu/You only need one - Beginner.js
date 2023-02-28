e7c4bbd2a1338c18f4a3d00b1b72970889a1cd70
user <gregorlitvinov@gmail.com>
2023-02-28 20:12:33 +0200

    countPositivesSumNegatives

--- 8kyu/You only need one - Beginner.js
+++ 8kyu/You only need one - Beginner.js
@@ -7,4 +7,11 @@ Return true if the array contains the value, false if not.
 */
 
 
-const check = (a, x) => a.includes(x);
+
+
+function check(a,x){
+  return a.includes(x);
+};
+
+
+//const check = (a, x) => a.includes(x);
